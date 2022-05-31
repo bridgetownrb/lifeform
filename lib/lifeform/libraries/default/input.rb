593a177202a39ae6907b1275c6439cde5c8e7c1f
Jared White <jared@jaredwhite.com>
2022-05-31 10:42:12 -0700

    Fields now support content blocks, conditional rendering

--- lib/lifeform/libraries/default/input.rb
+++ lib/lifeform/libraries/default/input.rb
@@ -22,6 +22,7 @@ module Lifeform
           @model = attributes.delete(:model)
           @model = form.model if form.model && @model.nil?
 
+          @if = attributes.delete(:if)
           attributes[:value] ||= value_for_model if form.model
           attributes[:name] = "#{model_name}[#{attributes[:name]}]" if @model
           attributes[:id] ||= attributes[:name].parameterize(separator: "_")
@@ -50,16 +51,20 @@ module Lifeform
           end
         end
 
-        def render_in(view_context)
+        def render_in(view_context, &block)
           @view_context = view_context
+          @content = block
+          return "" if !@if.nil? && !@if
+
           template
         end
 
-        def template
-          Papercraft.html do |wrapper_tag:, input_tag:, attributes:, field_type:, field_label:|
+        def template # rubocop:disable Metrics/AbcSize
+          Papercraft.html do |wrapper_tag:, input_tag:, attributes:, field_data:|
             field_body = proc {
-              emit(field_label) if field_label
-              send input_tag, type: field_type, **attributes
+              emit(field_data[:label]) if field_data[:label]
+              send input_tag, type: field_data[:type], **attributes
+              emit(field_data[:content]) if field_data[:content]
             }
             next field_body.call unless wrapper_tag
 
@@ -68,8 +73,11 @@ module Lifeform
             wrapper_tag: self.class.const_get(:WRAPPER_TAG),
             input_tag: self.class.const_get(:INPUT_TAG),
             attributes: attributes,
-            field_type: @field_type,
-            field_label: @label
+            field_data: {
+              type: @field_type,
+              label: @label,
+              content: @content && @view_context.capture(&@content)
+            }
           )
         end
       end
--- test/test_lifeform.rb
+++ test/test_lifeform.rb
@@ -5,6 +5,7 @@ require "test_helper"
 class TestForm < Lifeform::Form
   field :occupation, label: "Your Job", id: "your-occupation", required: true
   field :age, library: :shoelace, label: "Your Age"
+  field :noshow
 end
 
 class TestLifeform < Minitest::Test
@@ -28,16 +29,32 @@ class TestLifeform < Minitest::Test
     refute_nil ::Lifeform::VERSION
   end
 
+  def test_form_library
+    assert_equal :default, TestForm.library
+
+    level1 = Class.new(TestForm) do
+      library :shoelace
+    end
+
+    assert_equal :shoelace, level1.library
+
+    level2 = Class.new(level1)
+
+    assert_equal :shoelace, level2.library
+  end
+
   def test_form_output
     form_object = TestForm.new(url: "/path")
     document_root(form_object.render_in(self) do |f|
       [
         f.field(:occupation).render_in(self),
-        f.field(:age, value: 47).render_in(self)
+        f.field(:age, value: 47).render_in(self),
+        f.field(:noshow, if: false).render_in(self)
       ].join
     end)
 
     form = css_select("form").first
+    # puts form.to_html
 
     assert_equal "/path", form[:action]
     assert_equal "post", form[:method]
@@ -65,5 +82,7 @@ class TestLifeform < Minitest::Test
     assert_equal "text", sl_input[:type]
     assert_equal "age", sl_input[:name]
     assert_equal "47", sl_input[:value]
+
+    refute form.css("form-field")[2]
   end
 end
